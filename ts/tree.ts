1392c270efb8d2a900d918e9a1f012404154e401
Awesomebirds <dado2472@gmail.com>
2021-05-30 13:57:08 +0900

    전위 순회

---
+++ ts/tree.ts
@@ -0,0 +1,87 @@
+{
+  type Node<T> = null | {
+    body: T;
+    left: Node<T> | null;
+    right: Node<T> | null;
+  };
+
+  interface Tree<T> {
+    preorderReadTree(): void;
+  }
+
+  class TreeI<T> implements Tree<T> {
+    private node: Node<T> = null;
+
+    // constructor(body: T) {
+    //   this.node = {
+    //     body,
+    //     left: null,
+    //     right: null,
+    //   };
+    // }
+
+    preorderReadTree = (node?: Node<T>) => {
+      const currentNode = node || this.node;
+      console.log(currentNode.body);
+
+      currentNode.left && this.preorderReadTree(currentNode.left);
+      currentNode.right && this.preorderReadTree(currentNode.right);
+    };
+
+    addNode = (body: string) => {};
+
+    editTree = (node: Node<T>): void => {
+      this.node = node;
+    };
+  }
+
+  const myTree = new TreeI<string>();
+  const myNode: Node<string> = {
+    body: "a",
+    left: {
+      body: "b",
+      left: {
+        body: "d",
+        left: {
+          body: "h",
+          left: null,
+          right: null,
+        },
+        right: null,
+      },
+      right: {
+        body: "e",
+        left: {
+          body: "i",
+          left: null,
+          right: null,
+        },
+        right: {
+          body: "j",
+          left: null,
+          right: null,
+        },
+      },
+    },
+    right: {
+      body: "c",
+      left: {
+        body: "f",
+        left: null,
+        right: null,
+      },
+      right: {
+        body: "g",
+        left: null,
+        right: {
+          body: "k",
+          left: null,
+          right: null,
+        },
+      },
+    },
+  };
+
+  myTree.editTree(myNode);
+  myTree.preorderReadTree();
+}
