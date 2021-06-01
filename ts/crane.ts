55eff7097f7ee622503b7b3f12b209aca827c7ea
awesomebirds <dado2472@gmail.com>
2021-06-01 17:00:31 +0900

    완료!

--- ts/crane.ts
+++ ts/crane.ts
@@ -1,33 +1,37 @@
 class Crane {
   private board: Array<Array<number>>;
-  private stack: Array<number> = [];
 
   constructor(board: Array<Array<number>>) {
     this.board = board;
   }
 
   play = (moves: Array<number>): number => {
+    const stack: Array<number> = [];
     let count = 0;
 
     moves.forEach((move) => {
       move--;
-      this.board.forEach((line) => {
-        if (line[move] == 0) return; //비어있는지 검사
+      let isBreak = false;
 
-        if (this.stack && this.stack[this.stack.length - 1]) {
+      this.board.forEach((line) => {
+        if (line[move] == 0 || isBreak) return; //비어있는지 검사
+
+        if (stack && stack[stack.length - 1] == line[move]) {
           //스택의 맨 위 요소가 꺼낸 인형과 같을 경우
-          this.stack.pop(); //맨 위 요소를 제거함
+          stack.pop(); //맨 위 요소를 제거함
           count++; //카운트 증가
-          console.log(this.board);
         } else {
-          this.stack.push(line[move]); //스택에 꺼낸 인형을 넣어줌
+          stack.push(line[move]); //스택에 꺼낸 인형을 넣어줌
         }
 
         line[move] = 0; //인형을 꺼낸 공간은 비어있는 것으로 바꿈
+        console.log(this.board);
+        console.log(stack);
+        isBreak = true;
       });
     });
 
-    return count;
+    return count * 2;
   };
 }
 
