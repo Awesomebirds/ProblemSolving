a1be92071bf2f7613a2e2dd4e09b80c21001201e
awesomebirds <dado2472@gmail.com>
2021-06-05 12:51:39 +0900

    소문자 변환 + 두개씩 자르기

--- cpp/Jaccard.cpp
+++ cpp/Jaccard.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -6,6 +8,19 @@ int main() {
   string first, second; //첫번째 문자열, 두번째 문자열 정의
   cin >> first >> second; //문자열 입력받음
 
-  
+  //소문자 변환
+  for(int i = 0; i < first.length(); i++){
+    first[i] = tolower(first[i]);
+  }
+  for(int i = 0; i < second.length(); i++){
+    second[i] = tolower(second[i]);
+  }
+
+  vector<string>  firstMultiSet, secondMultilSet;
+  for(int i = 0; i < first.length() - 1; i++){
+    string el = first.substr(i, 2);
+    cout << el <<endl;
+  }
+
   return 0;
 }
