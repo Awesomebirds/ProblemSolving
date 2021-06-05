f4e38247d3c012d40c19dc19aeac03d5ac15cf8f
Awesomebirds <dado2472@gmail.com>
2021-06-05 14:49:41 +0900

    멀티셋 만들기 완료

--- cpp/Jaccard.cpp
+++ cpp/Jaccard.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+vector<string> makeMultiSet(string word) {
+  vector<string> multiSet;
+  for(int i = 0; i < word.length() - 1; i++){
+    string el = word.substr(i, 2);
+    int isContinue = 0;
+    for (int j = 0; j < el.length(); j++){
+      if(el[j] < 97 || el[j] > 122) isContinue = 1; //[a-z]검사
+    }
+    if(isContinue == 1) continue;
+    multiSet.push_back(el);
+  }
+  return multiSet;
+}
+
 int main() {
   string first, second; //첫번째 문자열, 두번째 문자열 정의
   cin >> first >> second; //문자열 입력받음
@@ -17,9 +31,16 @@ int main() {
   }
 
   vector<string>  firstMultiSet, secondMultilSet;
-  for(int i = 0; i < first.length() - 1; i++){
-    string el = first.substr(i, 2);
-    cout << el <<endl;
+  firstMultiSet = makeMultiSet(first);
+  secondMultilSet = makeMultiSet(second);
+  
+  //멀티셋 출력 (확인용)
+  for(int i = 0; i < firstMultiSet.size(); i++){
+    cout << firstMultiSet[i] << endl;
+  }
+
+  for(int i = 0; i < secondMultilSet.size(); i++){
+    cout << secondMultilSet[i] << endl;
   }
 
   return 0;
