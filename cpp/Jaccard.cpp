259cc9a9c7e2f5a5bd756b81afbdffec54659bce
Awesomebirds <dado2472@gmail.com>
2021-06-05 15:36:55 +0900

    자카드 완료

--- cpp/Jaccard.cpp
+++ cpp/Jaccard.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <time.h>
 
 using namespace std;
+
+int Jaccard(vector<string> firstMultiSet, vector<string> secondMultiSet){
+  int firstLength = firstMultiSet.size();
+  int secondLength = secondMultiSet.size();
+
+  vector<string> myIntersection;
+  for (int i = 0; i < firstMultiSet.size(); i++){
+    for(int j = 0; j < secondMultiSet.size(); j++){
+      if(firstMultiSet[i] == secondMultiSet[j]){
+        myIntersection.push_back(firstMultiSet[i]);
+        secondMultiSet.erase(secondMultiSet.begin() + j);
+        break;
+      }
+    }
+  }
+  
+  for(int i = 0; i < myIntersection.size(); i++){
+    cout << myIntersection[i] << endl;
+  }
+  double temp = (double) myIntersection.size() / (double) (firstLength + secondLength - myIntersection.size());
+  int jaccard = temp * 65536;
+  return jaccard;
+}
 
 vector<string> makeMultiSet(string word) {
   vector<string> multiSet;
@@ -20,7 +44,8 @@ vector<string> makeMultiSet(string word) {
 
 int main() {
   string first, second; //첫번째 문자열, 두번째 문자열 정의
-  cin >> first >> second; //문자열 입력받음
+  getline(cin, first);
+  getline(cin, second);
 
   //소문자 변환
   for(int i = 0; i < first.length(); i++){
@@ -33,15 +58,14 @@ int main() {
   vector<string>  firstMultiSet, secondMultilSet;
   firstMultiSet = makeMultiSet(first);
   secondMultilSet = makeMultiSet(second);
-  
-  //멀티셋 출력 (확인용)
-  for(int i = 0; i < firstMultiSet.size(); i++){
-    cout << firstMultiSet[i] << endl;
-  }
 
-  for(int i = 0; i < secondMultilSet.size(); i++){
-    cout << secondMultilSet[i] << endl;
-  }
+  clock_t start = clock(); //시간 측정 시작
+
+  int answer = Jaccard(firstMultiSet, secondMultilSet);
+
+  clock_t end = clock(); //  시간 측정 종료
+  cout << answer << endl; //정답 출력
+  cout << "실행시간: "<<(double)(end-start)/CLOCKS_PER_SEC  << "초" << "\n"; //시간 출력
 
   return 0;
 }
