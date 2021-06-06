980a64018a994f14e8e6a807e23da44d551b3060
awesomebirds <dado2472@gmail.com>
2021-06-07 02:41:18 +0900

    완주자 시작

---
+++ js/complete.js
@@ -0,0 +1,11 @@
+function solution(participant, completion) {
+  var answer = "";
+  participant.forEach((element) => {});
+  return answer;
+}
+
+const part = ["leo", "kiki", "eden"];
+const comp = ["eden", "kiki"];
+
+const answer = solution(part, comp);
+console.log(answer);
