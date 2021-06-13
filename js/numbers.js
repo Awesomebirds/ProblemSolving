3ce9fdcdebad4f46b5fec710357df95a29b700b0
Awesomebirds <dado2472@gmail.com>
2021-06-13 13:53:55 +0900

    문제시작

---
+++ js/numbers.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+  var answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {}
+  }
+
+  return answer;
+}
+z;
