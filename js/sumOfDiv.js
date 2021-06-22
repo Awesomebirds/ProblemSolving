49d3f63953fc6ce8870362ed134d76b8fa7c34c0
awesomebirds <dado2472@gmail.com>
2021-06-22 13:29:22 +0900

    약수의 합

--- js/sumOfDiv.js
+++ js/sumOfDiv.js
@@ -1,13 +1,14 @@
 function solution(n) {
   let answer = 0;
-  for (let i = 1; i * i <= n; i++) {
+  for (let i = 1; i * i < n; i++) {
     if (n % i === 0) {
       //i가 n의 약수
       answer += i; //나누는 수
       answer += n / i; //몫
     }
   }
+  if (n % Math.sqrt(n) === 0) answer += Math.sqrt(n);
   return answer;
 }
 
-console.log(solution(12));
+console.log(solution(25));
